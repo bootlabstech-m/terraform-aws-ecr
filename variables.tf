de62be9739c75e94cc89d1d10d484890547741d1
Silpashree <silpashree@bootlabstech.com>
2023-01-22 12:43:15 +0530

    added arn

--- variables.tf
+++ variables.tf
@@ -1,34 +1,32 @@
 variable "name" {
   description = "value"
   type        = string
-
 }
 variable "force_delete" {
   description = "value"
   type        = string
   default     = false
-
 }
 
 variable "image_tag_mutability" {
   description = "value"
   type        = string
-
 }
 variable "image_scanning_configuration" {
   description = "value"
   type        = string
   default     = true
-
 }
 variable "scan_on_push" {
   description = "value"
   type        = bool
   default = true
-
 }
 variable "region" {
   description = "value"
   type        = string
-
+}
+variable "role_arn" {
+  description = " The ARN of the IAM role"
+  type = string
 }
