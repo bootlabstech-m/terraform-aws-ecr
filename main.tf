520cdd20a82faff1c884232fcab1cedecdf3e768
HARDIK SINGHAL <singhal.hardik@mahindra.com>
2023-12-15 11:51:24 +0530

    renamed name variable

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 resource "aws_ecr_repository" "container_repository" {
   count                = var.no_of_ecrs
-  name                 = var.ecr_name[count.index]
-  force_delete         = var.force_delete
+  name                 = var.name[count.index]
   image_tag_mutability = var.image_tag_mutability
 
   image_scanning_configuration {
--- variables.tf
+++ variables.tf
@@ -2,29 +2,13 @@ variable "no_of_ecrs" {
   description = "value"
   type        = number
 }
-variable "ecr_name" {
+variable "name" {
   description = "value"
   type        = list(string)
 }
-variable "force_delete" {
-  description = "value"
-  type        = string
-  default     = false
-}
-
 variable "image_tag_mutability" {
   description = "value"
   type        = string
-}
-variable "image_scanning_configuration" {
-  description = "value"
-  type        = string
-  default     = true
-}
-variable "scan_on_push" {
-  description = "value"
-  type        = bool
-  default = true
 }
 variable "region" {
   description = "value"
