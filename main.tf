70b97543a6ee7ddc449336f9c7e20fc8935251b7
Bhargava Naidu <bhargava@bootlabstech.com>
2025-07-08 18:12:34 +0530

    new provider and cli version 5.46.0

--- main.tf
+++ main.tf
@@ -4,8 +4,14 @@ resource "aws_ecr_repository" "container_repository" {
   image_tag_mutability = var.image_tag_mutability
 
   image_scanning_configuration {
-    scan_on_push = true
+    scan_on_push = var.scan_on_push
   }
+  encryption_configuration {
+    encryption_type = var.encryption_type
+    kms_key         = var.kms_key_id != "" ? var.kms_key_id : null
+  }
+  force_delete = var.force_delete
+
     lifecycle {
     ignore_changes = [tags]
   }
--- variables.tf
+++ variables.tf
@@ -1,20 +1,47 @@
 variable "no_of_ecrs" {
-  description = "value"
+  description = "Number of ECR repositories to create"
   type        = number
 }
+
 variable "name" {
-  description = "value"
+  description = "List of names for the ECR repositories"
   type        = list(string)
 }
+
 variable "image_tag_mutability" {
-  description = "value"
+  description = "Specifies whether image tags are mutable or immutable. Allowed values: MUTABLE or IMMUTABLE"
   type        = string
 }
+
+variable "scan_on_push" {
+  description = "Whether to enable image scanning on push for the ECR repository"
+  type        = bool
+  default     = true
+}
+variable "encryption_type" {
+  description = "The encryption type to use for the repository. Supported values are AES256 or KMS."
+  type        = string
+  default     = "AES256"
+}
+
+variable "kms_key_id" {
+  description = "The ARN of the KMS key to use when encryption_type is KMS."
+  type        = string
+  default     = ""
+}
+
+variable "force_delete" {
+  description = "If true, deletes the repository even if it contains images"
+  type        = bool
+  default     = false
+}
+
 variable "region" {
-  description = "value"
+  description = "AWS region where the ECR repositories will be created"
   type        = string
 }
+
 variable "role_arn" {
-  description = " The ARN of the IAM role"
-  type = string
+  description = "The ARN of the IAM role used to authenticate and authorize Terraform to manage AWS resources"
+  type        = string
 }
